85fef122d13cdd800ff8fd5e2e45fd2e20c7cbce
Aaron <gabbert.aaron@gmail.com>
2024-03-19 05:50:12 -0500

    added lowerMd5

--- textUtils.ts
+++ textUtils.ts
@@ -1,4 +1,4 @@
-export {defangIp, defangDomain, lowerSha256}
+export {defangIp, defangDomain, lowerSha256, lowerMd5}
 
 function defangIp(text: string) {
     return text.replaceAll(/(\d{1,3}\.\d{1,3}\.\d{1,3})\.(\d{1,3}))/g, "$1[.]$2");
@@ -10,6 +10,12 @@ function defangDomain(text: string) {
 
 function lowerSha256(text: string) {
     return text.replace(/(\w{64})/g, function(match) {
+        return match.toLowerCase();
+    });
+}
+
+function lowerMd5(text: string) {
+    return text.replace(/(\w{32})/g, function(match) {
         return match.toLowerCase();
     });
 }
