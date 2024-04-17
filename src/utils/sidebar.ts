f9b250ba4225dd6961ca29e583b0fa706b393c48
Aaron <gabbert.aaron@gmail.com>
2024-04-17 06:21:25 -0500

    added some code to listen for file events

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -1,4 +1,4 @@
-import { App, ItemView, WorkspaceLeaf } from "obsidian";
+import { App, ItemView, TFile, WorkspaceLeaf } from "obsidian";
 
 export const VIEW_TYPE = "plugin-sidebar";
 
@@ -19,6 +19,22 @@ export class PluginSidebar extends ItemView {
         const container = this.containerEl.children[1];
         container.empty();
         container.createEl("h4", {text: "Plugin sidebar!"});
+        container.createEl("a", {href: "https://google.com", text: "GOOGLE"});
+    }
+
+    registerActiveFileListener() {
+        this.registerEvent(
+            this.app.vault.on('modify', async (file: TFile) => {
+                if (file === this.app.workspace.getActiveFile()) {
+                    await this.updateView(file);
+                }
+            })
+        )
+    }
+
+    async updateView(file: TFile) {
+        const fileContent = await this.app.vault.cachedRead(file);
+        // parse the file content and populate the sidebar
     }
 
     protected async onClose(): Promise<void> {
