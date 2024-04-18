d47c7b66c031981452c4a38ddcab096a880a4b6b
Aaron <gabbert.aaron@gmail.com>
2024-04-18 05:40:30 -0500

    getting matches on file update

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -1,10 +1,15 @@
 import { App, ItemView, TFile, WorkspaceLeaf } from "obsidian";
+import { DOMAIN_REGEX, IP_REGEX, extractMatches } from "./textUtils";
 
 export const VIEW_TYPE = "plugin-sidebar";
 
 export class PluginSidebar extends ItemView {
+    ips: string[];
+    domains: string[];
+
     constructor(leaf: WorkspaceLeaf) {
         super(leaf);
+        this.registerActiveFileListener();
     }
 
     getViewType(): string {
@@ -20,6 +25,11 @@ export class PluginSidebar extends ItemView {
         container.empty();
         container.createEl("h4", {text: "Plugin sidebar!"});
         container.createEl("a", {href: "https://google.com", text: "GOOGLE"});
+        const ips = container.createDiv({cls: "tree-item"});
+        ips.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "IP addresses"});
+        const ipChildren = ips.createDiv({cls: "tree-item-children"});
+        const ipChild = ipChildren.createDiv({cls: "tree-item"});
+        ipChild.createDiv({cls: "tree-item-self is-clickable", text: "test"})
     }
 
     registerActiveFileListener() {
@@ -34,7 +44,10 @@ export class PluginSidebar extends ItemView {
 
     async updateView(file: TFile) {
         const fileContent = await this.app.vault.cachedRead(file);
-        // parse the file content and populate the sidebar
+        this.ips = extractMatches(fileContent, IP_REGEX);
+        this.domains = extractMatches(fileContent, DOMAIN_REGEX);
+        console.log(this.ips);
+        console.log(this.domains);
     }
 
     protected async onClose(): Promise<void> {
