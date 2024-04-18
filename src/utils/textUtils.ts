9cf4ea27e0aede6e1e19cd23e0f2a1872b619ad4
Aaron <gabbert.aaron@gmail.com>
2024-04-18 05:40:30 -0500

    getting matches on file update

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -19,6 +19,10 @@ export {
     todayFolderStructure
 }
 
+// regex for possibly defanged values
+export const IP_REGEX = /(\d{1,3}\[?\.\]?\d{1,3}\[?\.\]?\d{1,3}\[?\.\]?\d{1,3})/gi
+export const DOMAIN_REGEX = /((([a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}(\[?\.\]?)?)+)\[?\.\]?((xn--)?([a-z0-9\-]{2,61}|[a-z0-9-]{2,30}\[?\.\]?[a-z]{2,})))/g;
+
 function todayLocalDate(): string {
     /**
      * @returns current local date as a string in format YYYY-MM-DD
@@ -73,7 +77,7 @@ function defangDomain(text: string): string {
     const httpString = /http(s?):\/\//g;
     const anyDomain = /(([a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.?)+)\.((xn--)?([a-z0-9\-]{2,61}|[a-z0-9-]{2,30}\.[a-z]{2,}))/g;
     let retval = text.replaceAll(httpString, "hxxp$1[://]");
-    retval = retval.replaceAll(anyDomain, "$1[.]$3");
+    retval = retval.replaceAll(DOMAIN_REGEX, "$1[.]$3");
     return retval;
 }
 
@@ -145,7 +149,7 @@ function replaceTemplateText(template: string, content: string, note: TFile, con
 
 function extractMacros(text: string): string[] {
     /**
-     * Extract macros 
+     * Extract macros in the format {{macro}}
      * @param text
      * @returns a unique list of macros in the text
      */
@@ -155,6 +159,12 @@ function extractMacros(text: string): string[] {
 }
 
 function extractMatches(text: string, pattern: RegExp): string[] {
+    /**
+     * Extract matches from the text for a given regex
+     * @param text
+     * @param pattern
+     * @returns an array of matches
+     */
     const matches = text.matchAll(pattern);
     return addUniqueValuesToArray([], matches);
 }
