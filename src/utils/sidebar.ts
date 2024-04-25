ab29481eb3b642314e1c3400619351bd5b56c13b
Aaron <gabbert.aaron@gmail.com>
2024-04-25 06:06:21 -0500

    refreshing works

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -11,6 +11,10 @@ export class PluginSidebar extends ItemView {
     domainEl: HTMLDivElement;
     hashEl: HTMLDivElement;
 
+    private sidebarContainerClass = "sidebar-container tree-item";
+    private listClass = "sidebar-list-item";
+    private listItemClass = this.listClass + " tree-item-self";
+
     constructor(leaf: WorkspaceLeaf) {
         super(leaf);
         this.registerActiveFileListener();
@@ -28,13 +32,13 @@ export class PluginSidebar extends ItemView {
         const container = this.containerEl.children[1];
         container.empty();
         container.createEl("h4", {text: "Extracted indicators!"});
-        const ipContainer = container.createDiv({cls: "sidebar-container tree-item"});
+        const ipContainer = container.createDiv({cls: this.sidebarContainerClass});
         ipContainer.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "IPs"});
         this.ipEl = ipContainer.createDiv({cls: "tree-item-children"});
-        const domainContainer = container.createDiv({cls: "sidebar-container tree-item"});
+        const domainContainer = container.createDiv({cls: this.sidebarContainerClass});
         domainContainer.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "Domains"})
         this.domainEl = domainContainer.createDiv({cls: "tree-item-children"});
-        const hashContainer = container.createDiv({cls: "sidebar-container tree-item"});
+        const hashContainer = container.createDiv({cls: this.sidebarContainerClass});
         hashContainer.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "Hashes"});
         this.hashEl = hashContainer.createDiv({cls: "tree-item-children"});
     }
@@ -56,31 +60,33 @@ export class PluginSidebar extends ItemView {
         this.domains = extractMatches(fileContent, DOMAIN_REGEX);
         this.hashes = extractMatches(fileContent, HASH_REGEX);
         const container = this.containerEl.children[1];
-        const els = container.getElementsByClassName('sidebar-list-item');
+        const els = container.getElementsByClassName(this.listClass);
         if (els && els.length > 0) {
             Array.from(els).forEach((el: HTMLObjectElement) => {
-                container.removeChild(el);
+                try {
+                    el.parentNode?.removeChild(el);
+                } catch { }
             });
         }
         
         this.ips.forEach((ip) => {
             console.log(`ip: ${ip}`);
             if (!ip) return;
-            const el = this.ipEl.createDiv({cls: "sidebar-list-item tree-item-self"}).createDiv({cls: "tree-item-inner", text: ip});
+            const el = this.ipEl.createDiv({cls: this.listItemClass}).createDiv({cls: "tree-item-inner", text: ip});
             const button = new ButtonComponent(el)
-                .setIcon('cat')
+                .setButtonText('VT')
                 .setClass('sidebar-button');
             return button;
         });
         this.domains.forEach((domain) => {
             console.log(`domain: ${domain}`);
             if (!domain) return;
-            this.domainEl.createDiv({cls: "sidebar-list-item tree-item-self"}).createDiv({cls: "tree-item-inner", text: domain});
+            this.domainEl.createDiv({cls: this.listItemClass}).createDiv({cls: "tree-item-inner", text: domain});
         });
         this.hashes.forEach((hash) => {
             console.log(`hash: ${hash}`);
             if (!hash) return;
-            this.hashEl.createDiv({cls: "sidebar-list-item tree-item-self"}).createDiv({cls: "tree-item-inner", text: hash});
+            this.hashEl.createDiv({cls: this.listItemClass}).createDiv({cls: "tree-item-inner", text: hash});
         });
     }
 
