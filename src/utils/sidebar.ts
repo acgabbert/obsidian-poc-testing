58eb5a5ba98aa1e74018ad3a36634bde86bb53cb
Aaron <gabbert.aaron@gmail.com>
2024-04-25 09:52:40 -0500

    reorg'ed clearSidebar

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -1,5 +1,6 @@
 import { ButtonComponent, ItemView, TFile, WorkspaceLeaf } from "obsidian";
 import { DOMAIN_REGEX, HASH_REGEX, IP_REGEX, extractMatches } from "./textUtils";
+import { removeElements } from "./domUtils";
 
 export const VIEW_TYPE = "plugin-sidebar";
 
@@ -14,12 +15,14 @@ export class PluginSidebar extends ItemView {
     private sidebarContainerClass = "sidebar-container tree-item";
     private listClass = "sidebar-list-item";
     private listItemClass = this.listClass + " tree-item-self";
+    private tableContainerClass = "table-container";
     private tableClass = "sidebar-table-row";
     private tdClass = "sidebar-table-item";
 
     constructor(leaf: WorkspaceLeaf) {
         super(leaf);
         this.registerActiveFileListener();
+        this.registerOpenFile();
     }
 
     getViewType(): string {
@@ -35,13 +38,13 @@ export class PluginSidebar extends ItemView {
         container.empty();
         container.createEl("h4", {text: "Extracted indicators!"});
         const ipContainer = container.createDiv({cls: this.sidebarContainerClass});
-        ipContainer.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "IPs"});
+        ipContainer.createEl("h4", {cls: "tree-item-self is-clickable mod-collapsible", text: "IPs"});
         this.ipEl = ipContainer.createDiv({cls: "tree-item-children"});
         const domainContainer = container.createDiv({cls: this.sidebarContainerClass});
-        domainContainer.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "Domains"})
+        domainContainer.createEl("h4", {cls: "tree-item-self is-clickable mod-collapsible", text: "Domains"})
         this.domainEl = domainContainer.createDiv({cls: "tree-item-children"});
         const hashContainer = container.createDiv({cls: this.sidebarContainerClass});
-        hashContainer.createDiv({cls: "tree-item-self is-clickable mod-collapsible", text: "Hashes"});
+        hashContainer.createEl("h4", {cls: "tree-item-self is-clickable mod-collapsible", text: "Hashes"});
         this.hashEl = hashContainer.createDiv({cls: "tree-item-children"});
     }
 
@@ -56,11 +59,22 @@ export class PluginSidebar extends ItemView {
         );
     }
 
+    registerOpenFile() {
+        this.registerEvent(
+            this.app.workspace.on('file-open', async (file: TFile) => {
+                console.log(`file opened: ${file.path}`);
+                if (file === this.app.workspace.getActiveFile()) {
+                    await this.updateView(file);
+                }
+            })
+        );
+    }
+
     addIndicatorEl(parentEl: HTMLElement, indicator: string): void {
         if (!indicator) return;
         const el = parentEl.createDiv({cls: this.listItemClass});
         el.createDiv({cls: "tree-item-inner", text: indicator});
-        const buttonEl = parentEl.createDiv({cls: "table-container"}).createEl("table").createEl("tr", {cls: this.tableClass});
+        const buttonEl = parentEl.createDiv({cls: this.tableContainerClass}).createEl("table").createEl("tr", {cls: this.tableClass});
         new ButtonComponent(buttonEl.createEl("td", {cls: this.tdClass}))
             .setButtonText('VT')
             .setClass('sidebar-button');
@@ -74,14 +88,10 @@ export class PluginSidebar extends ItemView {
     }
 
     clearSidebar(container: Element): void {
-        const els = container.getElementsByClassName(this.listClass);
-        if (els && els.length > 0) {
-            Array.from(els).forEach((el: HTMLObjectElement) => {
-                try {
-                    el.parentNode?.removeChild(el);
-                } catch { }
-            });
-        }
+        const listEls = container.getElementsByClassName(this.listClass);
+        const buttonEls = container.getElementsByClassName(this.tableContainerClass);
+        removeElements(listEls);
+        removeElements(buttonEls);
         return;
     }
 
