38fd7513a2bcc66a28169c885217877c8978fd6f
Aaron <gabbert.aaron@gmail.com>
2024-08-10 09:12:26 -0500

    kinda got an ugly poc working for apiresult appending

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
-import { RequestUrlParam, request } from "obsidian";
+import { App, Notice, RequestUrlParam, TFile, request } from "obsidian";
+import { appendToEnd } from "./editorUtils";
+import { VtResponse } from "./vt";
 
-export { apiRequest }
+export { apiRequest, appendResult }
 
 export type ApiVals = {
     baseUrl: string,
@@ -9,9 +11,25 @@ export type ApiVals = {
     headers?: Record<string, string>
 }
 
-async function apiRequest(vals: ApiVals, val: string, key: string): Promise<Object> {
+export const loadingMessage = 'Loading... ⏳';
+export const loadingMessage2 = 'Loading... ⌛';
+
+async function apiRequest(vals: ApiVals, val: string, key: string): Promise<VtResponse> {
     const headers = {'x-apikey': key};
     const vtParams = {url: vals.baseUrl + val, headers: headers, throw: true} as RequestUrlParam;
     const data = await request(vtParams);
-    return JSON.parse(data).data;
+    return JSON.parse(data).data as Promise<VtResponse>;
+}
+
+async function appendResult(result: Promise<VtResponse>, app: App, file: TFile) {
+    const loading = new Notice("Loading results... ⏳", 0);
+    result.then((data: VtResponse) => {
+        loading.hide();
+        console.log(`appending content to ${file.name}`);
+        appendToEnd(app, file, `\`\`\`ApiResult\n${data.type}\n\`\`\``);
+    }, (err) => {
+        loading.hide();
+        new Notice("Error calling API.");
+        console.log(err);
+    })
 }
