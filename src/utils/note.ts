f90c259a0d0f136bbe9a1b1d6bfe3554eae260cc
acgabbert <55032726+acgabbert@users.noreply.github.com>
2024-08-30 13:05:25 -0500

    Added the parser class

--- src/utils/note.ts
+++ src/utils/note.ts
@@ -18,3 +18,50 @@ export class PluginNote {
         );
     }
 }
+
+export interface MetadataField {
+	name: string;
+	regex: RegExp;
+}
+
+export class MarkdownParser {
+	[x: string]: any;
+	private fields: MetadataField[];
+	private parsedData: { [key: string]: string };
+
+	constructor(fields: MetadataField[]) {
+		this.fields = fields;
+		this.parsedData = {};
+		
+		return new Proxy(this, {
+			get(target, prop) {
+				if (prop in target) {
+					return target[prop as keyof MarkdownParser];	
+				}
+				return target.parsedData[prop as string];
+			}
+		}
+	}
+
+	parse(content: string): void {
+		this.parsedData = {};
+		this.fields.forEach(field => {
+			const match = field.regex.exec(content);
+			if (match && match[1]) {
+				this.parsedData[field.name] = match[1].trim();
+			}
+		});
+	}
+
+	get(fieldName: string): string | undefined {
+		return this.parsedData[fieldName];
+	}
+
+	addField(field: MetadataField): void {
+		this.fields.push(field);
+	}	
+
+	removeField(fieldName: string): void {
+		this.fields = this.fields.filter(f => f.name !== fieldName);
+	}
+}
