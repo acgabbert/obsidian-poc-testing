17302014d518d50ad034c16dc941d9f5d618c98f
Aaron <gabbert.aaron@gmail.com>
2024-09-05 09:11:00 -0500

    added more typing/static stuff

--- src/utils/sidebarReact.tsx
+++ src/utils/sidebarReact.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { App, ItemView, WorkspaceLeaf } from "obsidian";
+import { App, ItemView, TFile, WorkspaceLeaf } from "obsidian";
 import { Root, createRoot } from "react-dom/client";
 
 export const AppContext = createContext<App | undefined>(undefined);
@@ -12,27 +12,24 @@ type IocListProps = {
 }
 
 export const IocList: React.FC<IocListProps> = ({title, indicators}) => {
-    const indicatorList = indicators.map((item) => <div>{item}</div>)
-    return <details>
-        <summary>{title}</summary>
-        <div>{indicatorList}</div>
+    const indicatorList = indicators.map((item) => <div className="sidebar-list-item tree-item-self"><div className="tree-item-inner">{item}</div></div>)
+    return <details className="sidebar-container tree-item" open>
+        <summary className="tree-item-inner">{title}</summary>
+        <div className="tree-item-children">
+            {indicatorList}
+        </div>
     </details>
 }
-/*
-export function IocList({title, indicators}) {
-    const indicatorList = indicators.map((item) => <div>{item}</div>)
-    return <details>
-        <summary>{title}</summary>
-        <div>{indicatorList}</div>
-    </details>
-}
-*/
+
 export default function Sidebar() {
+    const ipList = ['8.8.8.8', '9.9.9.9'];
+    const domainList = ['facebook.com', 'google.com'];
+    const hashList = ['6F26C1696C909282D86B1A4F2CD8B41D5F6F30C9DAE5D316035F657D461C7E07'];
     return (
         <>
-            <IocList title="IPs" indicators={['asdf', 'qwerty']}/>
-            <IocList title="Domains" indicators={['asdf', 'qwerty']}/>
-            <IocList title="Hashes" indicators={['asdf', 'qwerty']}/>
+            <IocList title="IPs" indicators={ipList}/>
+            <IocList title="Domains" indicators={domainList}/>
+            <IocList title="Hashes" indicators={hashList}/>
         </>
     )
 }
