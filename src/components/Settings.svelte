2ef8eb2692d641e172bdd1031b74aa453ec831cd
Aaron <gabbert.aaron@gmail.com>
2024-09-12 05:37:03 -0500

    added basic settings checkbox

---
+++ src/components/Settings.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    //export let iocList: string[];
+    export let settings: boolean[];
+</script>
+
+<h4>Extracted Indicators</h4>
+{#each settings as setting}
+    <div>
+        <input type="checkbox" id="test" checked={setting}>
+        <label for="test">TEST!!</label>
+    </div>
+{/each}
--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,7 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 
 import MyPlugin from "main";
 import { defaultSites, type folderPrefs, removeDotObsidian, type searchSite } from "./utils";
+import Settings from "./components/Settings.svelte";
 
 export { DEFAULT_SETTINGS, MySettingTab };
 export type { MyPluginSettings };
@@ -30,6 +31,7 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 class MySettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
+	settingsView: Settings | undefined;
 
 	constructor(app: App, plugin: MyPlugin) {
 		super(app, plugin);
@@ -39,69 +41,11 @@ class MySettingTab extends PluginSettingTab {
 	async display(): Promise<void> {
 		const {containerEl} = this;
 
-		containerEl.empty();
-		const vault = this.app.vault;
-		const folders = vault.getAllFolders();
-        let topLevelFiles = (await vault.adapter.list('')).files;
-        topLevelFiles = removeDotObsidian(topLevelFiles);
-
-
-		new Setting(containerEl)
-			.setName('Root folder dropdown')
-			.setDesc('The folder to start searching from')
-			.addDropdown( (dropdown) => {
-				folders.forEach((folder) => {
-					dropdown.addOption(folder.path, folder.path);
-				})
-				dropdown.onChange(async (value) => {
-					this.plugin.settings.rootFolderDropdown = value;
-					await this.plugin.saveSettings();
-				});
-			});
-		new Setting(containerEl)
-			.setName('Code file')
-			.setDesc("The file where you'll store your code snippets")
-            .addDropdown((dropdown) => {
-                topLevelFiles.forEach((file) => {
-                    dropdown.addOption(file, file.replace(".md", ""));
-                });
-                dropdown.setValue(this.plugin.settings.codeFile);
-                dropdown.onChange(async (value) => {
-                    this.plugin.settings.codeFile = value;
-                    await this.plugin.saveSettings();
-                })
-            })
-		new Setting(containerEl)
-			.setName('VirusTotal API Key')
-			.addText((text) => {
-				text.setValue(this.plugin.settings.vtApiKey);
-				text.onChange(async (value) => {
-					this.plugin.settings.vtApiKey = value;
-					await this.plugin.saveSettings();
-				})
-			})
-		new Setting(containerEl)
-			.setName('AbuseIPDB API Key')
-			.addText((text) => {
-				text.setValue(this.plugin.settings.ipdbApiKey);
-				text.onChange(async (value) => {
-					this.plugin.settings.ipdbApiKey = value;
-					await this.plugin.saveSettings();
-				})
-			})
-		new Setting(containerEl)
-			.setName('Search Engines')
-			.setHeading();
-		this.plugin.settings.searchSites.forEach((site: searchSite) => {
-			new Setting(containerEl)
-				.setName(site.name)
-				.addToggle(toggle => toggle
-					.setValue(this.plugin.settings.searchSites[this.plugin.settings.searchSites.indexOf(site)].enabled)
-					.onChange(async (value) => {
-						this.plugin.settings.searchSites[this.plugin.settings.searchSites.indexOf(site)].enabled = value;
-						await this.plugin.saveSettings();
-					})
-				)
-		})
+		this.settingsView = new Settings({
+			target: containerEl,
+			props: {
+				settings: [true]
+			}
+		});
 	}
 }
